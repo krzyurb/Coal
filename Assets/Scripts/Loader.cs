07621de2ef46d0d4f41a7fe3354394bfe2fa90fa
krzyurb <krzyurb@gmail.com>
2018-03-23 10:55:43 +0100

    Tutorial 01

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -1,13 +1,21 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Loader : MonoBehaviour {
-	public GameObject gameManager;          //GameManager prefab to instantiate.
 
+	public GameObject gameManager;
+
+
+
+	// Use this for initialization
 	void Awake () {
-		//Check if a GameManager has already been assigned to static variable GameManager.instance or if it's still null
 		if (GameManager.instance == null)
-			//Instantiate gameManager prefab
-			Instantiate(gameManager);
+			Instantiate (gameManager);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
 	}
 }
