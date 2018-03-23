09edb845fcbb56b2c3010b2d4a74e4eb2371d1a4
krzyurb <krzyurb@gmail.com>
2018-03-23 16:38:58 +0100

    Real move

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -35,16 +35,7 @@ public abstract class MovingObject : MonoBehaviour {
         }
         return false;
     }
-
-	protected IEnumerator SmoothMovement(Vector3 end) {
-		float sqrRemainDistance = (transform.position - end).sqrMagnitude;
-		while (sqrRemainDistance > float.Epsilon) {
-			Vector3 newposition = Vector3.MoveTowards (rb2D.position, end, inverseMoveTime * Time.deltaTime);
-			rb2D.MovePosition(newposition);
-			sqrRemainDistance = (transform.position - end).sqrMagnitude;
-			yield return null;
-		}
-	}
+		
 
     protected virtual void AttemptMove <T> (int xDir, int yDir)
           where T : Component
@@ -59,7 +50,6 @@ public abstract class MovingObject : MonoBehaviour {
         if(!canMove && hitComponent != null)
             OnCantMove(hitComponent);
         
-
     }
 
     protected abstract void OnCantMove<T>(T component)
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,94 +4,108 @@ using UnityEngine;
 
 public class Player : MovingObject {
 
-    public int wallDamage = 1;
-    public int pointsPerFood = 10;
-    public int pointsPerSoda = 20;
-    public float restartLevelDelay = 1f;
-    private Animator animator;
-    private int food;
+	public int wallDamage = 1;
+	public int pointsPerFood = 10;
+	public int pointsPerSoda = 20;
+	public float restartLevelDelay = 1f;
+	private Animator animator;
+	private int food;
 
-    // Use this for initialization
-    protected override void Start () {
-        animator = GetComponent<Animator>();
-        food = GameManager.instance.playerFoodPoints;
+	private bool isVer = false;
+	private bool isHor = false;
+
+	// Use this for initialization
+	protected override void Start () {
+		animator = GetComponent<Animator>();
+//		food = GameManager.instance.playerFoodPoints;
+		food = 10;
 		base.Start ();
 	}
 
-    private void OnDisable()
-    {
-        GameManager.instance.playerFoodPoints = food;
-    
-    }
-
-    // Update is called once per frame
-    void Update () {
-//        if (!GameManager.instance.playersTurn)
-//            return;
-
-        int horizontal = 0;
-        int vertical = 0;
-
-        horizontal = (int)Input.GetAxisRaw("Horizontal");
-        vertical = (int)Input.GetAxisRaw("Vertical");
-
-        if (horizontal != 0)
-            vertical = 0;
-
-        if (horizontal != 0 || vertical != 0)
-            AttemptMove<Wall>(horizontal, vertical);
+	private void OnDisable()
+	{
+//		GameManager.instance.playerFoodPoints = food;
 	}
 
-    protected override void AttemptMove<T>(int xDir, int yDir)
-    {
-        food--;
-        base.AttemptMove<T>(xDir, yDir);
-        RaycastHit2D hit;
-        CheckIfGameOver();
-        GameManager.instance.playersTurn = false;
-    }
+	// Update is called once per frame
+	void Update () {
 
-    protected override void OnCantMove<T>(T component)
-    {
-        Wall hitWall = component as Wall;
-        hitWall.DamageWall(wallDamage);
-        animator.SetTrigger("playerChop");
-    }
+		int horizontal = 0;
+		int vertical = 0;
 
-    private void Restart()
-    {
-        Application.LoadLevel(Application.loadedLevel);
-    }
+		horizontal = (int)Input.GetAxisRaw("Horizontal");
+		vertical =   (int)Input.GetAxisRaw("Vertical");
 
-    public void LoseFood(int loss)
-    {
-        animator.SetTrigger("playerHit");
-        food -= loss;
-        CheckIfGameOver();
-    }
+		if (horizontal != 0)
+			vertical = 0;
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.tag == "Exit")
-        {
-            Invoke("Restart", restartLevelDelay);
-            enabled = false;
-        }
-        else if(other.tag == "Food")
-        {
-            food += pointsPerFood;
-            other.gameObject.SetActive(false);
-        } 
-        else if(other.tag == "Soda")
-        {
-            food += pointsPerSoda;
-            other.gameObject.SetActive(false);
-        }
-    }
+		if (!isHor && horizontal != 0 && vertical == 0) {
+			isHor = true;
+			transform.position += new Vector3 ((horizontal > 0) ? 1f : -1f, 0f, 0f);
+		}
 
-    private void CheckIfGameOver()
-    {
-        if (food <= 0)
-            GameManager.instance.GameOver();
-    }
+		if (!isVer && horizontal == 0 && vertical != 0) {
+			isVer = true;
+			transform.position += new Vector3 (0f, (vertical > 0) ? 1f : -1f, 0f);
+		}
+
+		if (horizontal == 0 && vertical == 0) {
+			isHor = false;
+			isVer = false;
+		}
+	}
+
+
+	protected override void AttemptMove<T>(int xDir, int yDir)
+	{
+		food--;
+		base.AttemptMove<T>(xDir, yDir);
+		RaycastHit2D hit;
+		CheckIfGameOver();
+//		GameManager.instance.playersTurn = false;
+	}
+
+	protected override void OnCantMove<T>(T component)
+	{
+		Wall hitWall = component as Wall;
+//		hitWall.DamageWall(wallDamage);
+//		animator.SetTrigger("playerChop");
+	}
+
+	private void Restart()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	public void LoseFood(int loss)
+	{
+		animator.SetTrigger("playerHit");
+		food -= loss;
+		CheckIfGameOver();
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Exit")
+		{
+			Invoke("Restart", restartLevelDelay);
+			enabled = false;
+		}
+		else if(other.tag == "Food")
+		{
+			food += pointsPerFood;
+			other.gameObject.SetActive(false);
+		} 
+		else if(other.tag == "Soda")
+		{
+			food += pointsPerSoda;
+			other.gameObject.SetActive(false);
+		}
+	}
+
+	private void CheckIfGameOver()
+	{
+//		if (food <= 0)
+//			GameManager.instance.GameOver();
+	}
 }
