fc56a83b102b74d858a9b6e5116f5be148ac8d33
krzyurb <krzyurb@gmail.com>
2018-04-10 12:11:28 +0200

    Cleanup GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,22 +8,20 @@ public class GameManager : MonoBehaviour {
 	public static GameManager instance = null;
 	public BoardManager boardScript;
 	public float turnDelay = 0.00f;
-	public int playerFoodPoints = 100;
+	public float levelStartDelay = 2f;
 	public int level = 1;
-	[HideInInspector] public bool playersTurn = true;
-
 	public int coalTotal  = 10;
+	[HideInInspector] public bool playersTurn = true;
 	private List<Enemy> enemies;
 	private bool enemiesMoving;
-	public float levelStartDelay = 2f;
 	private bool doingSetup = true;
 	private GameObject levelImage;
 	
 	void Awake () {
 		if (instance == null)
-		  instance = this;
+			instance = this;
 		else if (instance != this)
-		  Destroy (gameObject);
+			Destroy (gameObject);
 
 		DontDestroyOnLoad (gameObject);
 		enemies = new List<Enemy> ();
@@ -35,18 +33,11 @@ public class GameManager : MonoBehaviour {
 
 	void OnLevelWasLoaded (int index) {
 		level++;
-//	    if(level == 3) {
-//	        level = 0;
-//	    }
 		InitGame ();
 	}
 
 	void OnSceneLoaded (Scene previousScene, Scene newScene) {
 	    level++;
-
-//	    if (level == 3) {
-//	        level = 0;
-//	    }
 	}
 
 	public void InitGame () {
@@ -78,15 +69,14 @@ public class GameManager : MonoBehaviour {
 
 	IEnumerator MoveEnemies() {
 	    enemiesMoving = true;
-	    Debug.Log("enemies.Count  "+ enemies.Count);
 	    yield return new WaitForSeconds (turnDelay);
 
 		if (enemies.Count == 0)
 			yield return new WaitForSeconds (turnDelay);
 	
-	    for (int i = 0; i < enemies.Count; i++) {
+		for (int i = 0; i < enemies.Count; i++) {
 			if (enemies[i] != null)
-				enemies [i].MoveEnemy ();
+				enemies[i].MoveEnemy ();
 			yield return new WaitForSeconds (0.5f);
 	    }
 
